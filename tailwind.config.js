5231490cc32f5ef688da6ac6ff301e731d757183
abdulWahab <abdulwahabofficial705@gmail.com>
2022-10-04 18:24:50 +0500

    header font update

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,10 +65,7 @@ module.exports = {
       },
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-      workSans: ['Work Sans', 'sans-serif'],
-      MontSerif: ['Montserrat', 'sans-serif'],
+      'work-sans': ['Work Sans', 'sans-serif'],
     },
 
     letterSpacing: {
